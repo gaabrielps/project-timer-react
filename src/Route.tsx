70d296f57e6f1f1c981626b0ad8c9989ba809316
gaabrielps <araujolopesgabriel@gmail.com>
2022-11-29 20:46:05 -0300

    stylinghome

--- src/Route.tsx
+++ src/Route.tsx
@@ -1,5 +1,5 @@
 import { Route, Routes } from 'react-router-dom'
-import { DefautLayout } from './layouts/DefautLayout'
+import { DefautLayout } from './layouts/DefautLayouts/index'
 import { History } from './pages/History'
 import { Home } from './pages/Home'
 
